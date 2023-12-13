8d0531c6c3fdb9377f1ece71a4ecc7ee4bded02c
mehmet dogan <mehmetseymen91@gmail.com>
2023-12-14 01:46:27 +0300

    consulted ok

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -3,23 +3,25 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { TiDeleteOutline } from "react-icons/ti";
 
-const AppointmentList = ({ appointments,handleDelete  }) => {
+const AppointmentList = ({ appointments, handleDelete, handleDobleClick }) => {
   console.log(appointments);
-
 
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-2" style={{ color: "rgb(166, 18, 189)" }}>
         Appointment List
       </h3>
-      
-       {appointments.length === 0 && <img src="./img/appointment.jpg" alt="appointment" width="70%" />}
-      {appointments.map(({ id, patient, day, consulted, doctor ,}) => {
+
+      {appointments.length === 0 && (
+        <img src="./img/appointment.jpg" alt="appointment" width="70%" />
+      )}
+      {appointments.map(({ id, patient, day, consulted, doctor }) => {
         return (
           <div
             key={id}
             className={consulted ? "appointments consulted" : "appointments"}
             role="button"
+            onDoubleClick={()=>handleDobleClick(id)}
           >
             <Row className="justify-content-between align-items-center ">
               <Col md={6}>
@@ -31,7 +33,11 @@ const AppointmentList = ({ appointments,handleDelete  }) => {
                 <h6>Time:{new Date(day).toLocaleTimeString("tr")}</h6>
               </Col>
               <Col className="text-end">
-                <TiDeleteOutline className="text-danger fs-1 icons " type="button" onClick={()=>handleDelete(id)} />
+                <TiDeleteOutline
+                  className="text-danger fs-1 icons "
+                  type="button"
+                  onClick={() => handleDelete(id)}
+                />
               </Col>
             </Row>
           </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,38 @@
-import { useState } from "react"
-import AppointmentList from "../components/AppointmentList"
-import Doctors from "../components/Doctors"
-import {appointmentData} from "../helpers/Data"
+import { useState } from "react";
+import AppointmentList from "../components/AppointmentList";
+import Doctors from "../components/Doctors";
+import { appointmentData } from "../helpers/Data";
 
 const Home = () => {
- 
-  const [appointments,setAppointment]=useState(appointmentData)
+  const [appointments, setAppointment] = useState(appointmentData);
 
-  const handleAdd = (newAppointment) =>{
-      setAppointment([...appointments,newAppointment])
-  }
+  const handleAdd = (newAppointment) => {
+    setAppointment([...appointments, newAppointment]);
+  };
 
-  const handleDelete = (id)=>{
-    const filteredList = appointments.filter((item)=>(item.id !== id))
-    setAppointment(filteredList)
-  }
-       
+  const handleDelete = (id) => {
+    const filteredList = appointments.filter((item) => item.id !== id);
+    setAppointment(filteredList);
+  };
+
+  const handleDobleClick = (id) => {
+    const updateList = appointments.map((item) =>
+      item.id === id ? { ...item, consulted: !item.consulted } : item
+    );
+    setAppointment(updateList);
+  };
+
   return (
     <main className="text-center mt-2 justify-content-center m-auto ">
       <h1 className="display-5 text-danger">CLARUS HOSPITAL</h1>
-      <Doctors  handleAdd={handleAdd}/>
-      <AppointmentList appointments={appointments} handleDelete={handleDelete }/>
+      <Doctors handleAdd={handleAdd} />
+      <AppointmentList
+        appointments={appointments}
+        handleDelete={handleDelete}
+        handleDobleClick={handleDobleClick}
+      />
     </main>
-  )
-}
+  );
+};
 
 export default Home;
